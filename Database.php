0ba873b0bc4a164547b2c1a052c0e1c671cd24b8
Fernando Minardi <minardifer@gmail.com>
2024-02-28 18:13:10 -0300

    Ajuste de fechas, Desafío 1

--- Database.php
+++ Database.php
@@ -15,9 +15,9 @@ class Database
         $db->exec("INSERT INTO debts(id, lote, precio, clientID, vencimiento) VALUES (3,'00147',160000,135486, NULL)");
         $db->exec("INSERT INTO debts(id, lote, precio, clientID, vencimiento) VALUES (4,'00148',130000,123456, '2022-10-01')");
         $db->exec("INSERT INTO debts(id, lote, precio, clientID, vencimiento) VALUES (5,'00148',145000,123456, NULL)");
-        $db->exec("INSERT INTO debts(id, lote, precio, clientID, vencimiento) VALUES (6,'00148',190000,123456, '2022-12-01')");
-        $db->exec("INSERT INTO debts(id, lote, precio, clientID, vencimiento) VALUES (7,'00148',190000,123456, '2023-01-01')");
-        $db->exec("INSERT INTO debts(id, lote, precio, clientID, vencimiento) VALUES (8,'00148',190000,123456, '2023-02-01')");
+        $db->exec("INSERT INTO debts(id, lote, precio, clientID, vencimiento) VALUES (6,'00148',190000,123456, '2025-12-01')");
+        $db->exec("INSERT INTO debts(id, lote, precio, clientID, vencimiento) VALUES (7,'00148',190000,123456, '2025-01-01')");
+        $db->exec("INSERT INTO debts(id, lote, precio, clientID, vencimiento) VALUES (8,'00148',190000,123456, '2025-02-01')");
     }
 
     public static function getConnection()
